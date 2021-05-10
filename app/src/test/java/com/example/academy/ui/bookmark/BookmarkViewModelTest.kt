86dc80a29611ab5d713e590736ab0b53256252e2
david-hidayat <davidhidayat73@gmail.com>
2021-05-10 11:01:49 +0700

    Initial commit

---
+++ app/src/test/java/com/example/academy/ui/bookmark/BookmarkViewModelTest.kt
@@ -0,0 +1,21 @@
+package com.example.academy.ui.bookmark
+
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Test
+class BookmarkViewModelTest {
+
+    private lateinit var viewModel: BookmarkViewModel
+    @Before
+    fun setUp() {
+        viewModel = BookmarkViewModel()
+    }
+
+    @Test
+    fun getCourses() {
+        val courseEntities = viewModel.getBookmarks()
+        assertNotNull(courseEntities)
+        assertEquals(5, courseEntities.size)
+    }
+}
